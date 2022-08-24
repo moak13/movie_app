82f846a21cec605ef939bd62e13c0b548095fdd8
moak13 <michaelkalango@gmail.com>
2022-08-24 18:01:49 +0100

    wrapper view route setup

---
+++ lib/core/app/app.router.dart
@@ -0,0 +1,60 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StackedRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs, unused_import, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import '../../features/wrapper/wrapper_view.dart';
+
+class Routes {
+  static const String wrapperView = '/wrapper-view';
+  static const all = <String>{
+    wrapperView,
+  };
+}
+
+class StackedRouter extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(Routes.wrapperView, page: WrapperView),
+  ];
+  @override
+  Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, StackedRouteFactory>{
+    WrapperView: (data) {
+      return buildAdaptivePageRoute<dynamic>(
+        builder: (context) => const WrapperView(),
+        settings: data,
+      );
+    },
+  };
+}
+
+/// ************************************************************************
+/// Extension for strongly typed navigation
+/// *************************************************************************
+
+extension NavigatorStateExtension on NavigationService {
+  Future<dynamic> navigateToWrapperView({
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo(
+      Routes.wrapperView,
+      id: routerId,
+      preventDuplicates: preventDuplicates,
+      parameters: parameters,
+      transition: transition,
+    );
+  }
+}
--- lib/features/wrapper/view_model/wrapper_viewmodel.dart
+++ lib/features/wrapper/view_model/wrapper_viewmodel.dart
@@ -1,1 +1,3 @@
+import 'package:stacked/stacked.dart';
 
+class WrapperViewModel extends BaseViewModel {}
