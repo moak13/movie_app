cd677b22d4ec952452d7ce127570875fd5a28af3
moak13 <michaelkalango@gmail.com>
2022-08-24 20:28:48 +0100

    movie skeletal feature

--- lib/core/app/app.router.dart
+++ lib/core/app/app.router.dart
@@ -10,15 +10,18 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+import '../../features/movie/movie_view.dart';
 import '../../features/splash/splash_view.dart';
 import '../../features/wrapper/wrapper_view.dart';
 
 class Routes {
   static const String splashView = '/';
   static const String wrapperView = '/wrapper-view';
+  static const String movieView = '/movie-view';
   static const all = <String>{
     splashView,
     wrapperView,
+    movieView,
   };
 }
 
@@ -28,6 +31,7 @@ class StackedRouter extends RouterBase {
   final _routes = <RouteDef>[
     RouteDef(Routes.splashView, page: SplashView),
     RouteDef(Routes.wrapperView, page: WrapperView),
+    RouteDef(Routes.movieView, page: MovieView),
   ];
   @override
   Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
@@ -41,6 +45,12 @@ class StackedRouter extends RouterBase {
     WrapperView: (data) {
       return buildAdaptivePageRoute<dynamic>(
         builder: (context) => const WrapperView(),
+        settings: data,
+      );
+    },
+    MovieView: (data) {
+      return buildAdaptivePageRoute<dynamic>(
+        builder: (context) => const MovieView(),
         settings: data,
       );
     },
@@ -77,6 +87,22 @@ extension NavigatorStateExtension on NavigationService {
   }) async {
     return navigateTo(
       Routes.wrapperView,
+      id: routerId,
+      preventDuplicates: preventDuplicates,
+      parameters: parameters,
+      transition: transition,
+    );
+  }
+
+  Future<dynamic> navigateToMovieView({
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  }) async {
+    return navigateTo(
+      Routes.movieView,
       id: routerId,
       preventDuplicates: preventDuplicates,
       parameters: parameters,
