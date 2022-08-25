b628c2b222f153a193ed1b3588c2876deaf001a5
moak13 <michaelkalango@gmail.com>
2022-08-25 19:29:47 +0100

    blocked store

---
+++ lib/core/stores/blocked_store.dart
@@ -0,0 +1,55 @@
+import 'package:stacked/stacked.dart';
+
+import '../app/app.locator.dart';
+import '../app/app.logger.dart';
+import '../models/blocked_model.dart';
+import '../services/database_service.dart';
+
+const String _blockedTable = 'blocked';
+
+class BlockedStore extends BaseViewModel {
+  final _databaseService = locator<DatabaseService>();
+  final _log = getLogger('BlockedStore');
+
+  Stream<List<Blocked>> getStreamBlockedTitles() async* {
+    _log.i('streaming blocked titles');
+    List<Map<String, dynamic>> blockedResults =
+        await _databaseService.database!.query(_blockedTable);
+    yield blockedResults.map((e) => Blocked.fromJson(e)).toList();
+  }
+
+  Future<List<Blocked>> getBlockedTitles() async {
+    _log.i('fetching blocked titles');
+    List<Map<String, dynamic>> blockedResults =
+        await _databaseService.database!.query(_blockedTable);
+    return blockedResults.map((e) => Blocked.fromJson(e)).toList();
+  }
+
+  Future<void> addBlockedTitle({Blocked? title}) async {
+    _log.i('adding blocked title');
+    await _databaseService.database!.insert(_blockedTable, title!.toJson());
+    notifyListeners();
+  }
+
+  Future<void> removeBlockedTitle({Blocked? blockedMovie}) async {
+    _log.i('removing blocked title');
+    await _databaseService.database!.delete(_blockedTable,
+        where: "Title = ?", whereArgs: [blockedMovie?.title]);
+    notifyListeners();
+  }
+
+  Future<bool> isBlocked({String? title}) async {
+    _log.i('checking blocked state for $title');
+    bool status = false;
+    var records = await _databaseService.database!
+        .query(_blockedTable, where: "Title = ?", whereArgs: [title]);
+    if ((records).isEmpty) {
+      status = false;
+      notifyListeners();
+    } else {
+      status = true;
+      notifyListeners();
+    }
+    return status;
+  }
+}
