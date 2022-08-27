6f63d2ccab6b39ece370de6e9afe06da886f2e18
moak13 <michaelkalango@gmail.com>
2022-08-27 17:55:15 +0100

    added gesture detection to search view to enable keyboard dismissing when tapped

--- lib/features/search/search_view.dart
+++ lib/features/search/search_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../core/utils/context_util.dart';
 import '../../core/utils/size_manager.dart';
 import '../../core/utils/string_util.dart';
 import 'view_model/search_viewmodel.dart';
@@ -82,10 +83,15 @@ class SearchView extends StatelessWidget {
             ),
           );
         }
-        return Scaffold(
-          backgroundColor: theme.scaffoldBackgroundColor,
-          body: Stack(
-            children: children,
+        return GestureDetector(
+          onTap: () {
+            ContextUtils.hideKeyboard(context);
+          },
+          child: Scaffold(
+            backgroundColor: theme.scaffoldBackgroundColor,
+            body: Stack(
+              children: children,
+            ),
           ),
         );
       },
