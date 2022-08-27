e7846f8557714240722031ac5c9d27bd41d87209
moak13 <michaelkalango@gmail.com>
2022-08-27 20:24:01 +0100

    lifted view model using getParentViewModel

--- lib/features/search/search_view.dart
+++ lib/features/search/search_view.dart
@@ -8,13 +8,19 @@ import 'view_model/search_viewmodel.dart';
 import 'widgets/info_box.dart';
 import 'widgets/search_button.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
   const SearchView({Key? key}) : super(key: key);
 
+  @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     SizeMg.init(context);
+
     return ViewModelBuilder<SearchViewModel>.reactive(
       viewModelBuilder: () => SearchViewModel(),
       builder: (
@@ -22,59 +28,7 @@ class SearchView extends StatelessWidget {
         SearchViewModel model,
         Widget? child,
       ) {
-        List<Widget> children = [
-          Column(
-            children: [
-              Row(
-                children: [
-                  Text(
-                    'Search',
-                    style: theme.textTheme.titleLarge?.copyWith(
-                      fontSize: SizeMg.text(50),
-                    ),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: ListView(
-                  padding: EdgeInsets.only(
-                    top: SizeMg.height(30),
-                    left: SizeMg.width(10),
-                    right: SizeMg.width(10),
-                  ),
-                  children: [
-                    const InfoBox(),
-                    SizedBox(
-                      height: SizeMg.height(50),
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(
-                        hintText: 'Search for a movie',
-                      ),
-                      onSubmitted: (value) {
-                        if (StringUtil.isEmpty(value)) {
-                          return;
-                        }
-                        model.actionSetTitle(value);
-                      },
-                    ),
-                    SizedBox(
-                      height: SizeMg.height(300),
-                    ),
-                  ],
-                ),
-              ),
-              SearchButton(
-                onTap: () {
-                  model.actionSearchMovie(title: model.searchText);
-                },
-              ),
-              SizedBox(
-                height: SizeMg.height(10),
-              ),
-            ],
-          ),
-        ];
+        List<Widget> children = [content];
 
         if (model.isBusy) {
           children.add(
@@ -95,6 +49,62 @@ class SearchView extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+
+  Widget get content {
+    var theme = Theme.of(context);
+    var model = getParentViewModel<SearchViewModel>(context);
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              'Search',
+              style: theme.textTheme.titleLarge?.copyWith(
+                fontSize: SizeMg.text(50),
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: ListView(
+            padding: EdgeInsets.only(
+              top: SizeMg.height(30),
+              left: SizeMg.width(10),
+              right: SizeMg.width(10),
+            ),
+            children: [
+              const InfoBox(),
+              SizedBox(
+                height: SizeMg.height(50),
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Search for a movie',
+                ),
+                onSubmitted: (value) {
+                  if (StringUtil.isEmpty(value)) {
+                    return;
+                  }
+                  model.actionSetTitle(value);
+                },
+              ),
+              SizedBox(
+                height: SizeMg.height(300),
+              ),
+            ],
+          ),
+        ),
+        SearchButton(
+          onTap: () {
+            model.actionSearchMovie(title: model.searchText);
+          },
+        ),
+        SizedBox(
+          height: SizeMg.height(10),
+        ),
+      ],
     );
   }
 }
